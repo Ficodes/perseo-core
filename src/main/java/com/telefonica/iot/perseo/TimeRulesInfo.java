dac2f6235940d79c25b739d91d284faad4e39501
Carlos Blanco <cblanco@ficodes.com>
2018-09-10 15:54:53 +0200

    clean

--- src/main/java/com/telefonica/iot/perseo/TimeRulesInfo.java
+++ src/main/java/com/telefonica/iot/perseo/TimeRulesInfo.java
@@ -6,7 +6,6 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
